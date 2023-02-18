8a8500b301db9002d91de8ecfa27fa0168839d45
Mary <89971822+Mary-fox@users.noreply.github.com>
2023-02-18 13:29:01 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = [].concat(...bracketsConfig);
+  const open = brackets.filter(( _, i) => i % 2 === 0);
+  const close = brackets.filter((_, i) => i % 2 !== 0);
+
+
+    let stack = [];
+    for (let i=0; i<str.length; i++) {
+      let currentSymbol = str[i];
+
+        if (close.includes(currentSymbol)) {
+          let topElement = stack[stack.length-1];
+          if (close.indexOf(currentSymbol) === open.indexOf(topElement)) {
+            stack.pop()
+          } else {
+            stack.push(currentSymbol)
+          }
+    
+        } else if (open.includes(currentSymbol)) {
+          stack.push(currentSymbol);
+      }
+    
+  }
+  return stack.length === 0 ? true : false;
+    
 }
